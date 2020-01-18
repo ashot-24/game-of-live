01c9b5fb4b7c78dbc74082d3366b5b32e8778d38
Alexandr <alexandr.mirz12@gmail.com>
2020-01-18 19:26:50 +0400

    Initial commit

---
+++ class.js
@@ -0,0 +1,25 @@
+
+class Grass extends LivingCreature{
+    mul() {
+        this.multiply++;
+        if (this.multiply == 5) {
+            //Հետազոտում է շրջապատը, որոնում դատարկ տարածքներ
+            var fundCords = this.getDirections(0);
+            var cord = random(fundCords);
+            if (cord) {
+                var x = cord[0];
+                var y = cord[1];
+
+                //Ավելացնում է նոր խոտ խոտերի զանգվածում
+                var norXot = new Grass(x, y);
+                grassArr.push(norXot);
+
+                //Ավելացնում է նոր խոտի մասին գրառում հիմնական matrix-ում 
+                matrix[y][x] = 1;
+                this.multiply = 0;
+            }
+        }
+    }
+
+}
+
